b8970913b694f4cea46313e99ba9e801ca948148
pedro leinar <pedroleinar@hotmail.com>
2021-04-26 07:09:05 -0300

    add stripe

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,7 +1,17 @@
 import { SubscribeButton } from '../SubscribeButton';
 import styles from './styles.module.scss';
 
-export const Home = () => {
+type Product = {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+};
+
+const numberFormatter = (number: number) =>
+  new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(number);
+
+export const Home = ({ product }: Product) => {
   return (
     <main className={styles.contentContainer}>
       <section className={styles.hero}>
@@ -10,8 +20,8 @@ export const Home = () => {
           News about the <span>React</span> world.
         </h1>
         <p>
-          Get access to tall the publications <br />
-          <span>for $9.90 month</span>
+          Get access to all the publications <br />
+          <span>for {numberFormatter(product.amount)} month</span>
         </p>
         <SubscribeButton />
       </section>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,39 @@
 import Head from 'next/head';
+import { GetServerSideProps } from 'next';
 import { Home } from '../components/Home';
+import { stripe } from '../services/stripe';
 
-export default function Index() {
+type Product = {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+};
+
+export default function Index(props: Product) {
   return (
     <>
       <Head>
         <title>Home | ig.news</title>
       </Head>
-      <Home />
+      <Home product={props.product} />
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1IkQgEFvv4fIhzAZVcEe8IM2', {
+    expand: ['product'],
+  });
+
+  const product = {
+    priceId: price.id,
+    amount: price.unit_amount / 100,
+  };
+
+  return {
+    props: {
+      product,
+    },
+  };
+};
