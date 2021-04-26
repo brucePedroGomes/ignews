80b9b6b238124f1077774e52105cb64f898764f2
pedro leinar <pedroleinar@hotmail.com>
2021-04-26 03:41:16 -0300

    global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,12 @@
+import Head from 'next/head';
+
 export default function Home() {
-  return <h1>Hello world</h1>;
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <h1>Hello world</h1>
+    </>
+  );
 }
