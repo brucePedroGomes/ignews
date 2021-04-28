2766067beca5108c4b717237968ffa1f9871267c
pedro leinar <pedroleinar@hotmail.com>
2021-04-28 03:20:15 -0300

    feat: sign in with GitHub

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,15 +1,20 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 import styles from './styles.module.scss';
+import { signIn, useSession, signOut } from 'next-auth/client';
 
 export const SignInButton = () => {
-  const isUserLogged = true;
+  const [session] = useSession();
 
   return (
-    <button type="button" className={styles.signInButton}>
-      <FaGithub color={isUserLogged ? '#04d361' : '#eba417'} />
-      {isUserLogged ? 'Pedro Leinar' : 'Sing in with GitHub'}
-      {isUserLogged && <FiX color="#737380" className={styles.closeIcon} />}
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => (session ? signOut() : signIn())}
+    >
+      <FaGithub color={session ? '#04d361' : '#eba417'} />
+      {session ? session.user.name : 'Sing in with GitHub'}
+      {session && <FiX color="#737380" className={styles.closeIcon} />}
     </button>
   );
 };
